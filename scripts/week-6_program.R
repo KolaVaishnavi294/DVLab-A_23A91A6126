a5ba59154606247668e36edccc173a246bd522d2
venkatjee <venkatjee27@gmail.com>
2026-02-26 12:38:53 +0530

    week-6_program

--- scripts/week-6_program.R
+++ scripts/week-6_program.R
@@ -17,8 +17,40 @@ plot(diamonds$carat,diamonds$price,col=rgb(0,0,1,0.1),pch=16,
 #hexbin using base r
 install.packages('hexbin')
 library(hexbin)
-hb->hexbin(diamond$carat,diamond$price,xbins=40)
+hb<-hexbin(diamonds$carat,diamonds$price,xbins=40)
 plot(hb,main="Hexbin Plot")
 
 #Basic Hexbin
+install.packages('ggplot2')
+library(ggplot2)
 ggplot(diamonds,aes(x=carat,y=price))+geom_hex()
+
+#labeled hexbin plot
+ggplot(diamonds,aes(carat,price))+geom_hex()+
+  labs(title="Hexagons",x="Carat",y="Price")+
+  theme_minimal()
+
+ggplot(diamonds,aes(carat,price))+geom_hex(bins=10)+theme_minimal()
+
+#add color palatte
+ggplot(diamonds,aes(carat,price))+geom_hex(bins=40)+
+  scale_fill_gradient(low="skyblue",high="red")+
+  theme_minimal()
+
+#color meaning: light->fewer diamonds,dark->dense region
+#professional palette
+ggplot(diamonds,aes(carat,price))+geom_hex(bins=35)+scale_fill_viridis_c()+
+  theme_minimal()
+
+#add legend title
+ggplot(diamonds,aes(carat,price))+geom_hex(bins=20)+scale_fill_viridis_c(name="Count")+
+  theme_minimal()+labs(title="Density Structure of Diamonds",x="Carat",y="Price")+
+  theme_minimal()
+
+#faceted hexbin
+ggplot(diamonds,aes(carat,price))+geom_hex()+scale_fill_viridis_c()+
+  facet_wrap(~cut)+theme_minimal()
+
+#log Scale hexbin
+ggplot(diamonds,aes(carat,price))+geom_hex()+scale_fill_viridis_c()+
+  scale_y_log10()+theme_minimal()
